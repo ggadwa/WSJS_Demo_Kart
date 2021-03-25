c2d2f31393c579b0f182bb00b6d4a5534d81a944
Brian Barnes <ggadwa@charter.net>
2021-03-25 01:29:17 -0400

    03_25_2021 (Start of Scripting Change)

---
+++ html/demo_kart.js
@@ -0,0 +1,19 @@
+import ProjectClass from '../../../code/main/project.js';
+
+export default class DemoClass extends ProjectClass
+{
+    mapCube(name)
+    {
+        return(null);
+    }
+
+    mapEffect(name)
+    {
+        return(null);
+    }
+
+    mapEntity(name)
+    {
+        return(null);
+    }
+}
